559138e866f78e0c85a62e8be5a868d063b0ca28
ivkvitko <ivkvitko@8bitov.com>
2017-09-05 23:42:42 +0300

    add news

--- src/AdminBundle/Admin/NewsAdmin.php
+++ src/AdminBundle/Admin/NewsAdmin.php
@@ -13,7 +13,6 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NewsAdmin extends AbstractAdmin
 {
