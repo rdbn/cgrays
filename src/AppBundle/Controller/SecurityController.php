dcef3e5fe122ff7f76a0f314f4846091a4b44246
ivkvitko <ivkvitko@8bitov.com>
2017-09-10 19:18:53 +0300

    add user administration

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -13,6 +13,8 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class SecurityController extends Controller
 {
+    const SWITCH_USER = 'uuid_switch_user';
+
     /**
      * @param Request $request
      * @Route("/login", name="login")
@@ -39,17 +41,35 @@ class SecurityController extends Controller
      */
     public function switchUserAction(Request $request, $steamId)
     {
+        $tokenStorage = $this->get('security.token_storage');
         $authChecker = $this->get('security.authorization_checker');
-        $session = $request->getSession();
-        if ($authChecker->isGranted('ROLE_ALLOWED_TO_SWITCH')) {
-            if ($session->has('uuid_switch_user')) {
-                $steamId = $session->get('uuid_switch_user');
-            } else {
-                $session->set('uuid_switch_user', $this->getUser()->getSteamId());
-            }
 
-            $em = $this->get('doctrine.orm.entity_manager');
-            $user = $em->getRepository(User::class)
+        if ($authChecker->isGranted('ROLE_ALLOWED_TO_SWITCH')) {
+            $user = $this->get('doctrine.orm.entity_manager')->getRepository(User::class)
+                ->findOneBy(['steamId' => $steamId]);
+
+            $request->getSession()->set(self::SWITCH_USER, $tokenStorage->getToken()->getUser()->getSteamId());
+
+            $token = new UsernamePasswordToken($user, '', 'frontend', $user->getRoles());
+            $tokenStorage->setToken($token);
+        }
+
+        return $this->redirectToRoute('app.skins.main');
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/switch/exit", name="app.security.switch_exit")
+     * @return RedirectResponse
+     */
+    public function exitSwitchUserAction(Request $request)
+    {
+        $session = $request->getSession();
+        if ($session->has(self::SWITCH_USER)) {
+            $steamId = $session->get(self::SWITCH_USER);
+            $session->remove(self::SWITCH_USER);
+
+            $user = $this->get('doctrine.orm.entity_manager')->getRepository(User::class)
                 ->findOneBy(['steamId' => $steamId]);
 
             $token = new UsernamePasswordToken($user, '', 'frontend', $user->getRoles());
